28dadc78e8b306c6e27bd2592fa7dbc32b7c3096
Dan_Ostap <84470259+DanOstap@users.noreply.github.com>
2024-07-11 20:00:40 +0200

    Add project files.

---
+++ QuestionnaireBack/Models/QuestionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace QuestionnaireBack.Models
+{
+    public class QuestionRequest
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public int Rate { get; set; }
+    }
+}
---
+++ QuestionnaireBack/Service/QuestionmaireHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using QuestionnaireBack.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace QuestionnaireBack.Service
+{
+    public class QuestionmaireHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            string username = Context.User.Identity.Name;
+            Console.WriteLine($"User connected: {username}");
+
+            await base.OnConnectedAsync();
+        }
+        public  async Task AddToGroup(string groupName) {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+        public async Task SendMessage(QuestionRequest request, string groupName)
+        {
+            await Clients.Groups(groupName).SendAsync("ReceiveMessage",request.Name, request.Message, request.Rate);
+            Console.WriteLine($"Cotnex: {request.Name} {request.Message} \n  ConnectionId : {Context.ConnectionId} \n  User: {Context.User.Identity.Name}  UserIdentifier: {Context.UserIdentifier}");
+        }
+    }
+}
